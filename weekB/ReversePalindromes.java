9f4343ed5883e93a9710cd254d4a2293a6e1b08d
tayoung3 <tyoung3@umassd.edu>
2019-06-20 10:32:59 -0400

    Now with less unneccesary arrays

--- weekB/ReversePalindromes.java
+++ weekB/ReversePalindromes.java
@@ -4,54 +4,53 @@ import java.util.Arrays;
 
 public class ReversePalindromes {
 	public static void main(String[] args) {
-		System.out.println(recursiveReverse("notapalindrome"));
-		System.out.println(recursiveIsAPalindrome("dogeeseseegod"));
+		System.out.println(recursiveReverse("also not a palindrome"));
+		System.out.println(recursiveIsAPalindrome("doninemeninterpretninemeninod"));
 	}
-	
+
 	public static boolean recursiveIsAPalindrome(String str) {
-		char[] breakdown = str.toCharArray();
 		int length = str.length();
-		//base case
-		if(length == 1 || length == 0) {
+		// base case
+		if (length == 1 || length == 0) {
 			return true;
 		}
-		if(breakdown[0] == breakdown[length-1]) {
-			return recursiveIsAPalindrome(String.valueOf(Arrays.copyOfRange(breakdown, 1, length -1)));
-		}
-		else
+		if (str.charAt(0) == str.charAt(length - 1)) {
+			return recursiveIsAPalindrome(str.substring(1, length - 1));
+		} else
 			return false;
 	}
-	
+
 	public static String recursiveReverse(String str) {
-		char[] breakdown = str.toCharArray();
-		//base case
+		int length = str.length();
+		// base case
 		if (str.length() == 1) {
 			return str;
 		}
-		
-		return str.charAt(str.length() - 1) + recursiveReverse(String.valueOf(Arrays.copyOf(breakdown, str.length() - 1)));
+
+		return str.charAt(str.length() - 1)
+				+ recursiveReverse(str.substring(0, length -1));
 	}
-	
+
 	public static String reverse(String str) {
-		if(isAPalindrome(str)) {
+		if (isAPalindrome(str)) {
 			return str + " Palindrome";
 		}
 		int length = str.length();
 		char[] reversal = str.toCharArray();
-		for(int i = 0; i < length; i++) {
+		for (int i = 0; i < length; i++) {
 			char temp = str.charAt(i);
-			reversal[i] = str.charAt(length- 1 - i);
-			reversal[length - i - 1] = temp;	
+			reversal[i] = str.charAt(length - 1 - i);
+			reversal[length - i - 1] = temp;
 		}
 		return String.valueOf(reversal);
 	}
-	
+
 	public static boolean isAPalindrome(String str) {
 		char[] checker = str.toCharArray();
 		int length = str.length();
 		boolean pali = true;
 		for (int i = 0; i < length; i++) {
-			if(checker[i] == checker[length - 1 - i]) {
+			if (checker[i] == checker[length - 1 - i]) {
 				continue;
 			} else {
 				pali = false;
